d909bca5a6aac1cc1d3e8501ec6a711132fb06e3
Erik Dalén <erik.gustav.dalen@gmail.com>
2018-07-24 15:49:23 +0200

    Release 0.2.2

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use std::{thread, time};
 
 fn main() {
     let matches = App::new("ecs-run")
-        .version("0.2.1")
+        .version("0.2.2")
         .author("Erik Dalén <erik.gustav.dalen@gmail.com>")
         .setting(clap::AppSettings::TrailingVarArg)
         .arg(
